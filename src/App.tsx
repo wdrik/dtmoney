30ef8221e626b6d25fe0410788270cff1dd4130f
wdrik <w.drik@outlook.com>
2021-04-04 15:23:41 -0300

    feat: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import styled from 'styled-components';
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
     <div className="App">
       <h1>Hello world!</h1>
+      <GlobalStyle />
     </div>
   );
 }
